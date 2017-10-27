e6956c6c5417dfd12a95aec9c7c43cb4c579588e
Dinesh Kumar <dineshkumarbudhiraja@gmail.com>
2017-10-27 07:11:49 +0530

    added 2 tables of 7

--- JavaProject1/src/com/dk/main/Main.java
+++ JavaProject1/src/com/dk/main/Main.java
@@ -5,11 +5,18 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for (int i = 5; i<= 50 ; i+=5)
+		for (int i = 7; i<= 70 ; i+=7)
 		{
 			System.out.println(i);
 		}
 		//added 1
+		
+		for (int i = 7; i<= 70 ; i+=7)
+		{
+			System.out.println(i);
+		}
+		//added one more table
+		
 
 	}
 
