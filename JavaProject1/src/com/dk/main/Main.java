612068c17abc896d2bf73ec46903c54f3e90ff87
Dinesh Kumar <dineshkumarbudhiraja@gmail.com>
2017-10-27 07:17:57 +0530

    reverted to table of 5

--- JavaProject1/src/com/dk/main/Main.java
+++ JavaProject1/src/com/dk/main/Main.java
@@ -5,7 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		for (int i = 7; i<= 70 ; i+=7)
+		for (int i = 5; i<= 50 ; i+=5)
 		{
 			System.out.println(i);
 		}
